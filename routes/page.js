8a6aa98e9e494176e7f6407cf8e6dfdd99f52056
jdwns96 <jdwns96>
2022-05-11 16:28:32 +0900

    [UPDATE] toast UI

--- routes/page.js
+++ routes/page.js
@@ -5,6 +5,11 @@ const Post = require("../models/post");
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+  res.locals.user = req.user; // 모든 라우터에 다 들어가는 변수는 여기에 넣을수 있다. 미들웨어의 특성
+  next();
+});
+
 router.route("/").get(isLoggedIn, async (req, res) => {
   const post = await Post.findAll({ where: { id: req.user.id } });
   console.log(post);
@@ -31,6 +36,7 @@ router.route("/post/:id").get(isLoggedIn, async (req, res) => {
   console.log(post);
   res.render("post.html", {
     title: "메모장",
+    post: post,
   });
 });
 

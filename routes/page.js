821ad863df7d5dc8d59213c06a14f334de6b8f5f
jdwns96 <jdwns96>
2022-05-13 17:37:23 +0900

    [UPDATE] front pagination

--- routes/page.js
+++ routes/page.js
@@ -11,7 +11,7 @@ router.use((req, res, next) => {
 });
 
 router.get("/", isLoggedIn, async (req, res) => {
-  let page = req.query.page || 1; // 페이지 가 없을 경우
+  let page = req.query.page || 1; // 페이지 가 없을 경우 page == 1
 
   let limit = 5;
   let offset = 0;
@@ -19,37 +19,45 @@ router.get("/", isLoggedIn, async (req, res) => {
     offset = limit * (page - 1);
   }
   try {
+    // 페이지 전체 조회
+    const Posts = await Post.findAll({ where: { UserId: req.user.id } });
+    let allPostCnt = Posts.length;
+    // 선택된 페이지 조회
     const post = await Post.findAll({
       where: { UserId: req.user.id },
       offset,
       limit,
     });
+    const totalPage = Math.ceil(allPostCnt === 0 ? 1 : allPostCnt / limit);
 
     if (Number(page) < 1) return res.redirect("/"); // 만약 0보다 작을 경우
-    if (post.length === 0) return res.redirect("/"); // 게시글이 존재하지 않을경우
+    if (totalPage === 1 && Number(page) > 1) return res.redirect("/"); // 전체 페이지가 1일 경우
+    if (totalPage !== 1 && Number(page) > totalPage) return res.redirect("/"); // 전체 페이지보다 넘어가는 경우
 
     res.render("index.html", {
       title: "메모장",
       datas: post,
+      page,
+      totalPage,
     });
   } catch (error) {
     res.redirect("/");
   }
 });
 
-router.route("/login").get(isNotLoggedIn, (req, res) => {
+router.get("/login", isNotLoggedIn, (req, res) => {
   res.render("login.html", {
     title: "로그인",
   });
 });
 
-router.route("/join").get(isNotLoggedIn, (req, res) => {
+router.get("/join", isNotLoggedIn, (req, res) => {
   res.render("join.html", {
     title: "회원가입",
   });
 });
 
-router.route("/post/:id").get(isLoggedIn, async (req, res) => {
+router.get("/post/:id", isLoggedIn, async (req, res) => {
   const post = await Post.findOne({ where: { id: req.params.id } });
   if (!post) {
     return res.redirect("/");
